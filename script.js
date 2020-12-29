2fa2d5e4a659d703751ebb5c1cec71119be58c0b
cjoynerw <cjoynerw@gmail.com>
2020-12-29 14:02:18 -0600

    rendered map on current location and implemented multiple points

--- script.js
+++ script.js
@@ -15,6 +15,33 @@ navigator.geolocation.getCurrentPosition(
   function (position) {
     const { latitude } = position.coords;
     const { longitude } = position.coords;
+
+    const coords = [latitude, longitude];
+
+    const map = L.map("map").setView(coords, 13);
+
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    map.on("click", function (mapEvent) {
+      const { lat, lng } = mapEvent.latlng;
+
+      L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup(
+          L.popup({
+            maxWidth: 250,
+            minWidth: 200,
+            autoClose: false,
+            closeOnClick: false,
+            className: "running-popup",
+          })
+        )
+        .setPopupContent("Workout")
+        .openPopup();
+    });
   },
   function () {
     alert(`Could not get your position.`);
